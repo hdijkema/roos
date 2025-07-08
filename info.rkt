c568f621d2462ed56236760de3eb8f41f73afa98
Hans Dijkema <hans@dijkewijk.nl>
2025-07-08 17:00:57 +0200

    Version to 0.6

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define pkg-authors '(hnmdijkema))
-(define version "0.55")
+(define version "0.6")
 (define license 'Apache-2.0)
 (define collection "roos")
 (define pkg-desc "An OO Framework for Racket")
