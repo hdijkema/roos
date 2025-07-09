52792e5e909ed035e58f21875ed066e632580bbb
Hans Dijkema <hans@dijkewijk.nl>
2025-07-09 17:30:19 +0200

    .

--- info.rkt
+++ info.rkt
@@ -1,15 +1,17 @@
 #lang info
 
 (define pkg-authors '(hnmdijkema))
-(define version "0.8.0")
+(define version "0.8.1")
 (define license 'Apache-2.0)
-(define collection 'roos)
+(define collection "roos")
 (define pkg-desc "An OO Framework for Racket")
 
 (define scribblings
-  '(("scribblings/roos.scrbl" (multi-page) (library) "roos")
+  '(
+    ("scribblings/roos.scrbl" (multi-page) (library) "roos")
     ("scribblings/class.scrbl" (multi-page) (library) "roos/class")
-    ))
+    )
+  )
 
 (define deps
   '("base"))
